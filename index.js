f26e46eca81bfd58eb3dea6283dc8edd058248d0
hmsmiraz <hmsmiraz64729@gmail.com>
2023-12-20 12:58:59 +0600

    added new

--- index.js
+++ index.js
@@ -150,6 +150,10 @@ async function run() {
       res.send(result);
     });
 
+    app.post('/user', async (req, res)=>{
+      const user = req.body;
+      
+    })
     app.delete("/users/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
